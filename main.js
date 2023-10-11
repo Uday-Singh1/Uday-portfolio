b927ca834f532cf4bc630ed0c17fabba3e79c52d
Uday-Singh G <91188742+Uday-Singh1@users.noreply.github.com>
2023-10-11 12:17:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -164,7 +164,7 @@ class LanguageToggle {
             this.skilldescription1.textContent = "I am able to write Semantic HTML code and structure the codes well for a clearer experience.";
             this.skilldescription2.textContent = "I'm a big fan of design, so I really like CSS, not only creating beautiful elements but also beautiful animations.";
             this.skilldescription3.textContent = "Scss has taught me how to divide my CSS and therefore work on my projects in a categorized manner.";
-            this.skilldescription4.textContent = "I find JavaScript very interesting, especially because of the countless possibilities it offers, it really takes your project to a completely different level and it is really a challenge sometimes, which I like.";
+            this.skilldescription4.textContent ="I'm captivated by JavaScript's limitless potential to elevate projects to new heights, even when it presents challenging moments, which I enjoy.";
             this.skilldescription5.textContent = "I am a big fan of designing, I always try to make everything as unique as possible so that it really radiates my personality, of course I can work well if there are requirements for the design in terms of layout.";
             this.skilldescription6.textContent = "In my spare time, in addition to programming, I also often work on video/photo editing, mainly with Adobe After Effects and Premiere Pro, I really enjoy making those types of projects.";
 
@@ -217,7 +217,7 @@ class LanguageToggle {
             this.skilldescription1.textContent = "Ik ben instaat om Semantische html code te schrijven en de codes goed te structuren voor een overzichtelijkere ervaring.";
             this.skilldescription2.textContent = "Ik ben heel erg fan van design dus ik kan me goed vinden in css,om niet alleen mooie elementen te maken maar ook mooie animaties.";
             this.skilldescription3.textContent = "Scss heeft me geleerd hoe ik mijn css kan verdelen en dus ook gecategoriseerd kan gaan werken aan mijn projecten.";
-            this.skilldescription4.textContent = "Ik vind javascript heel interresant, vooral wegens de ontelbare mogelijkheden dat het bied, het neemmt je project echt op een hele andere niveau en het is ook echt een uitdaging soms wat ik dus fijn vind.";
+            this.skilldescription4.textContent ="Ik ben gefascineerd door de onbegrensde mogelijkheden van JavaScript om projecten naar nieuwe hoogten te tillen, zelfs als het soms uitdagend is, wat ik leuk vind.";
             this.skilldescription5.textContent = "Ik ben heel erg fan van designing, ik probeer alles altijd zo uniek mogelijk te maken dat het dus ook echt mijn personaliteit uitstraalt, uiteraard kan ik goed werken als er eisen zijn aan het design wat betreft opmaak.";
             this.skilldescription6.textContent = "In mijn vrije tijd naast het programmeren ben ik ook vaak bezig met video/foto editing voornamelijk met Adobe After Effects en Premiere Pro, ik vind het heel erg leuk om ook dat soort projecten te maken.";
 
